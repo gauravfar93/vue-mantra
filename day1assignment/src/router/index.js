dd4260fd14c5c8d7fb73a4b06ece8be66672e7ec
gauravfar93 <gauravfar93@gmail.com>
2024-01-08 19:16:08 +0530

    props is being used and v modal is also used to get the form data

--- day1assignment/src/router/index.js
+++ day1assignment/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import SignupView from '../views/SignupView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +16,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: SignupView
     }
   ]
 })
